b1c2b9fd7df39a933291f62fe6c2153d017fadb6
steevenakintilo <steeven-charles-edward.akintilo@epitech.eu>
2024-04-13 17:55:24 +0100

    [ADD] Better searching for giveaway

--- giveaway.py
+++ giveaway.py
@@ -15,11 +15,14 @@ import yaml
 from random import randint
 import random
 
+import undetected_chromedriver as uc 
+
+
 import traceback
 from selenium.webdriver.common.keys import Keys  
 
 import sys 
-
+import logging
 
 MINTIME = 0
 MAXTIME = 0
@@ -27,11 +30,18 @@ MAXTIME = 0
 class Scraper:
     
     wait_time = 5
-    options = webdriver.FirefoxOptions()
-    options.add_argument('headless')
+    options = webdriver.ChromeOptions()
     options.add_argument("--log-level=3")  # Suppress all logging levels
-    driver = webdriver.Firefox(options=options)
-
+    # options.add_experimental_option(
+    # "prefs", {"credentials_enable_service": False, "profile.password_manager_enabled": False})
+    # ua = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36'
+    # options.add_argument(f'--user-agent={ua}') 
+    # driver = uc.Chrome(options=options)
+    
+    options.add_argument('headless')    
+    driver = webdriver.Chrome(options=options)  # to open the chromedriver    
+    
+    #options.headless = False
 
 def write_into_file(path, x):
     with open(path, "a") as f:
@@ -53,27 +63,37 @@ def insta_giveaway(S,i,page_nb):
     file_urls = print_file_info("urls.txt").split("\n")
     S.driver.get(f"https://www.jeu-concours.biz/concours-instagram.php?page={page_nb}")
     cookiesbtn = "/html/body/div[14]/div[2]/div[1]/div[2]/div[2]/button[1]/p"
-    element =  WebDriverWait(S.driver,3).until(
-        EC.presence_of_element_located((By.XPATH, cookiesbtn)))
-
-    element.click()
-    time.sleep(1)
+    try:
+        element =  WebDriverWait(S.driver,7).until(
+            EC.presence_of_element_located((By.XPATH, cookiesbtn)))
+        element.click()
+    
+    except:
+       time.sleep(1)
+       S.driver.refresh()
+       element =  WebDriverWait(S.driver,7).until(
+            EC.presence_of_element_located((By.XPATH, cookiesbtn)))
+       element.click()
+    
+       
+    time.sleep(0.1)
     ggw = f"/html/body/section/div/div/div[1]/div[2]/article[{str(i)}]/nav/ul/li[4]/a"
-    element =  WebDriverWait(S.driver,5).until(
+    element =  WebDriverWait(S.driver,15).until(
         EC.presence_of_element_located((By.XPATH, ggw)))
     
     S.driver.execute_script("arguments[0].scrollIntoView();", element)
-    time.sleep(0.5)
+    time.sleep(0.1)
     element.click()
 
     new_window = S.driver.window_handles[-1]
     S.driver.switch_to.window(new_window)
-    time.sleep(2)
+    time.sleep(0.1)
 
     new_window_url = S.driver.current_url
-
+    
     if new_window_url not in urls and new_window_url not in file_urls:
        write_into_file("urls.txt",new_window_url + "\n")
+       write_into_file("recent_urls.txt",new_window_url + "\n")
        urls.append(new_window_url)
 
     # Close the new window
@@ -83,9 +103,6 @@ def insta_giveaway(S,i,page_nb):
     original_window = S.driver.window_handles[0]
     S.driver.switch_to.window(original_window)
 
-    time.sleep(2)
-
-
 def fck():
   try:
     S = Scraper()
@@ -94,5 +111,7 @@ def fck():
     S.driver.close()
   except:
     S.driver.close()
+    pass
+    
 
 fck()
--- start.py
+++ start.py
@@ -2,12 +2,23 @@ from os import system
 import traceback
 import time
 
+from giveaway import print_file_info , reset_file
+
+MAX = 50
+
+reset_file("recent_urls.txt")
+
 try:
   for j in range(1,5):
     for i in range(1,51):
       try:
-        system(f"python search.py {i} {j}")
+        system(f"python giveaway.py {i} {j}")
+        print("Nb of giveaway found " , len(print_file_info("recent_urls.txt").split("\n")))
+        if len(print_file_info("recent_urls.txt").split("\n")) > 50:
+          exit()
       except:
         traceback.print_exc()
+        exit()
 except:
+  exit()
   pass
